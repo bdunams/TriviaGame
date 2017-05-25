cbb639a1d856a96ba32f129359673695a0bf1f8c
Brian Dunams <Brian Dunams>
2017-05-25 18:01:46 -0400

    version 2.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   
+  // event listeners
   $("#start").on('click', trivia.startGame);
   $(document).on('click' , '.option', trivia.guessChecker);
   
@@ -11,36 +12,58 @@ var trivia = {
   incorrect: 0,
   unanswered: 0,
   currentSet: 0,
-  result: false,
-  timer: 15,
+  timer: 20,
   timerOn: false,
   timerId : '',
+  // questions options and answers data
   questions: {
-    q1: 'What month?',
-    q2: '2',
-    q3: '3',
-    q4: '4'
+    q1: 'Who the only American president to be unanimously elected?',
+    q2: 'Who was the first president to call his residence in Washington, D.C. the "White House"?',
+    q3: 'Who was the first president to hold a press conference on television?',
+    q4: 'Who was the first president to campaign by telephone?',
+    q5: 'Who was the first American president to name an African American to his cabinet?',
+    q6: 'Who was the 16th president of the United States?',
+    q7: "Which president established Yellowstone as the nation's first national park on March 1, 1872"
   },
   options: {
-    q1: ['May', 2, 3, 4, 5],
-    q2: [1, 2, 3, 4, 5],
-    q3: [1, 2, 3, 4, 5],
-    q4: [1, 2, 3, 4, 5]
+    q1: ['Abraham Lincoln', 'Barack Obama', 'James Madison', 'George Washington'],
+    q2: ['Thomas Jefferson', 'George Washington', 'Theodore Roosevelt', 'John Quincy Adams'],
+    q3: ['Lyndon B. Johnson', 'Franklin D. Roosevelt', 'Barack Obama', 'John F. Kennedy'],
+    q4: ['Theodore Roosevelt', 'William McKinley', 'Harry S. Truman', 'Franklin D. Roosevelt'],
+    q5: ['Lyndon B. Johnson','James Monroe','Abraham Lincoln','Millard Fillmore'],
+    q6: ['James K. Polk','James Monroe','Zachary Taylor','Abraham Lincoln'],
+    q7: ['Grover Cleveland', 'John Tyler', 'Ulysses S. Grant','Herbert Hoover']
   },
   answers: {
-    q1: 'May',
-    q2: '2',
-    q3: '3',
-    q4: '4'
+    q1: 'George Washington',
+    q2: 'Theodore Roosevelt',
+    q3: 'John F. Kennedy',
+    q4: 'William McKinley',
+    q5: 'Lyndon B. Johnson',
+    q6: 'Abraham Lincoln',
+    q7: 'Ulysses S. Grant'
   },
   // trivia methods
   // method to initialize game
   startGame: function(){
+    // restarting game results
+    trivia.currentSet = 0;
+    trivia.correct = 0;
+    trivia.incorrect = 0;
+    trivia.unanswered = 0;
+    clearInterval(trivia.timerId);
+    
+    // show game section
+    $('#game').show();
+    
+    //  empty last results
+    $('#results').html('');
+    
     // show timer
     $('#timer').text(trivia.timer);
     
     // remove start button
-    $('#start').remove();
+    $('#start').hide();
     
     // ask first question
     trivia.nextQuestion();
@@ -63,8 +86,11 @@ var trivia = {
     var questionContent = Object.values(trivia.questions)[trivia.currentSet];
     $('#question').text(questionContent);
     
+    // an array of all the user options for the current question
+    var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+    
     // creates all the trivia guess options in the html
-    $.each(Object.values(trivia.options)[trivia.currentSet], function(index, key){
+    $.each(questionOptions, function(index, key){
       $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
     })
     
@@ -79,13 +105,30 @@ var trivia = {
           $('#timer').addClass('last-seconds');
         }
     }
-    //  time has run out and increment unanswered, run result
+    // the time has run out and increment unanswered, run result
     else if(trivia.timer === -1){
       trivia.unanswered++;
       trivia.result = false;
       clearInterval(trivia.timerId);
       resultId = setTimeout(trivia.guessResult, 3000);
       $('#results').html('<h3>Times up! The answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
+    }
+    // if all the questions have been shown end the game, show results
+    else if(trivia.currentSet === Object.keys(trivia.questions).length){
+      
+      // adds results of game (correct, incorrect, unanswered) to the page
+      $('#results')
+        .html('<h3>Here are the results of the game!</h3>'+
+        '<p>Correct: '+ trivia.correct +'</p>'+
+        '<p>Incorrect: '+ trivia.incorrect +'</p>'+
+        '<p>Unaswered: '+ trivia.unanswered +'</p>'+
+        '<p>Please play again!</p>');
+      
+      // hide game sction
+      $('#game').hide();
+      
+      // show start button to begin a new game
+      $('#start').show();
     }
     
   },
@@ -95,49 +138,45 @@ var trivia = {
     // timer ID for gameResult setTimeout
     var resultId;
     
-    console.log($(this).text());
-    console.log(Object.values(trivia.answers)[trivia.currentSet]);
+    // the answer to the current question being asked
+    var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
     
-    // if the text of the option matches the answer of the current question
-    if($(this).text() === Object.values(trivia.answers)[trivia.currentSet]){
+    // if the text of the option picked matches the answer of the current question, increment correct
+    if($(this).text() === currentAnswer){
+      // turn button green for correct
+      $(this).addClass('btn-success').removeClass('btn-info');
       
       trivia.correct++;
-      trivia.result = true;
       clearInterval(trivia.timerId);
       resultId = setTimeout(trivia.guessResult, 3000);
-      console.log(trivia.correct);
       $('#results').html('<h3>Correct Answer!</h3>');
     }
+    // else the user picked the wrong option, increment incorrect
     else{
+      // turn button clicked red for incorrect
+      $(this).addClass('btn-danger').removeClass('btn-info');
       
       trivia.incorrect++;
-      trivia.result = false;
       clearInterval(trivia.timerId);
       resultId = setTimeout(trivia.guessResult, 3000);
-      console.log(trivia.incorrect);
-      $('#results').html('<h3>Sorry the answer was '+ Object.values(trivia.answers)[trivia.currentSet] +'</h3>');
+      $('#results').html('<h3>Sorry, the answer was '+ currentAnswer +'</h3>');
     }
     
   },
-  // 
+  // method to remove previous question results and options
   guessResult : function(){
+    
+    // increment to next question set
     trivia.currentSet++;
     
-    if(trivia.result === true){
-      //$(this).removeClass('btn-info').addClass('btn-success');
-      $('.option').remove();
-      trivia.nextQuestion();
-      $('#results h3').remove();
-    }
-    else if(trivia.result === false){
-      //$(this).removeClass('btn-info').addClass('btn-success');
-      $('.option').remove();
-      trivia.nextQuestion();
-      $('#results h3').remove();
-    }
+    // remove the options and results
+    $('.option').remove();
+    $('#results h3').remove();
     
-  },
-  
+    // begin next question
+    trivia.nextQuestion();
+     
+  }
 
 }
   
